dddadfbd59e5bb19785ffa2dac07cfbcab952417
RenukaBaby <dhanamalirenuka@duck.com>
2026-05-25 01:33:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 750,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
+      nodeIntegration: false,    // Set to false for security (standard web apps)
+      contextIsolation: true,    // Safe context sandboxing
+      devTools: true             // Switch to false if you want to hide developer tools (Ctrl+Shift+I)
     }
   });
 
-  win.loadFile("index.html");
+  // Load your local frontend file
+  win.loadFile('index.html');
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
